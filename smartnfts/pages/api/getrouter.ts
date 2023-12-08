4c927d250f4046b27ec5044cbcfec5d1754dfccd
Kamal Nayan <geniusamansingh@gmail.com>
2023-12-08 21:30:29 +0530

    1inch portfolio apis

---
+++ smartnfts/pages/api/getrouter.ts
@@ -0,0 +1,27 @@
+
+import axios from "axios";
+
+export default async function handler(req : NextApiRequest, res : NextApiResponse)  {
+  const { chainid, params } = req.query;
+
+  if (!chainid) {
+    return res.status(400).json({ error: "Missing 'url' parameter in the request." });
+  }
+
+  const url = `https://api.1inch.dev/swap/v5.2/${chainid}/quote`;
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.APIONEINCH}`,
+    },
+    params: params ? JSON.parse(params) : {},
+  };
+
+  try {
+    const response = await axios.get(url, config);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
