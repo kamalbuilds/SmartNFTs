2a1baf4439cf038e3fd3a41878779ce604bf2008
kamalbuilds <geniusamansingh@gmail.com>
2023-12-10 06:46:07 +0530

    trying to fix the bugs

--- smartnfts/next.config.js
+++ smartnfts/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  }
+  "compilerOptions": {
+    "lib": [
+      "esnext",
+    ],
+    "module": "commonjs",
+    "target": "esnext",
+}}
 
 module.exports = nextConfig;
